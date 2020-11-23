5f68d3da12b80f0ece2835d43a041a96bf9ea99d
Devansh Dubey <69635552+dubeyji0110@users.noreply.github.com>
2020-11-23 23:59:23 +0530

    Updated New Scripts

--- movieAPI.js
+++ movieAPI.js
@@ -1,40 +1,61 @@
 const api = "https://www.omdbapi.com/?i=tt3896198&apikey=be41af31";
-const searchbox = document.querySelector('.search-box');
 
-const button = document.querySelector('.btn');
-button.addEventListener('click', setQuery);
+const header = document.querySelector('#header');
+const submitButton = document.querySelector('#submit');
+const mainContent = document.querySelector('#main-content');
+const input = document.querySelector('#queryBox');
+const filterBox = document.querySelector('.filterBox');
 
-function setQuery(evt) {
-    getResults(searchbox.value);
+submitButton.addEventListener('click', start);
+
+function start() {
+    header.classList.add('toTop');
+    filterBox.style.display = 'block';
+    getData(input.value);
+    input.value = '';
 }
 
-function getResults(query) {
-    fetch(`${api}&s=${query}`)
-        .then(movie => {
-            return movie.json();
-        }).then((movie) => {
-            var content = document.querySelector('#content');
+function getData(val) {
+    fetch(`${api}&s=${val}`)
+        .then(data => {
+            return data.json();
+        }).then((data) => {
+            // console.log(data)
 
-            var box = content.lastElementChild;
-            while (box) {
-                content.removeChild(box);
-                box = content.lastElementChild;
+            let boxes = mainContent.lastElementChild;
+            while (boxes) {
+                mainContent.removeChild(boxes);
+                boxes = mainContent.lastElementChild;
             }
 
-            for (var a = 0; a < movie.Search.length; a++) {
+            for (let a = 0; a < data.Search.length; a++) {
+
                 let box = document.createElement('div');
-                box.setAttribute('class', 'box');
+                box.setAttribute('class', `card ${data.Search[a].Type}`);
+                mainContent.appendChild(box);
 
-                let content = document.getElementById('content');
-
-                content.appendChild(box);
-
-                box.innerHTML = `<div class="title">Title: ${movie.Search[a].Title}, (${movie.Search[a].Year})</div>
-                <div class="type">Type: ${movie.Search[a].Type}</div>
-                <div class="poster"><img src="${movie.Search[a].Poster}" alt="Poster not available"></img></div>
-                <div class="moreinfo"><a href="https://www.imdb.com/title/${movie.Search[a].imdbID}" target="_blank">Watch</a></div>`;
+                box.innerHTML = `<h3 class="title">${data.Search[a].Title}, (${data.Search[a].Year})</h3><h6 class="type">TYPE: ${data.Search[a].Type}</h6><div class="poster"><img src="${data.Search[a].Poster}" alt="Poster Not Available"></div><a href="https://www.imdb.com/title/${data.Search[a].imdbID}" class="watch" target="_blank">WATCH</a>`;
             }
-        }).catch(movie => {
-            alert(`Permssion Denied By your Browser! or Movie Not Found!`);
+        }).catch(e => {
+            alert(`Movie Not Found!`);
+            header.classList.remove('toTop')
+            filterBox.style.display = 'none';
         });
 }
+
+// jQuery started here
+$(document).ready(function () {
+    $('.list').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'All') {
+            $('.card').show('1000');
+        } else {
+            $('.card').not('.' + value).hide('1000');
+            $('.card').filter('.' + value).show('1000');
+        }
+    })
+
+    $('.list').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+})
